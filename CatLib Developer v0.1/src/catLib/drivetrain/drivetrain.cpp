dd06a187b30ea48aaf241d2de98674da45725fda
StevenQQQian <114100226+StevenQQian@users.noreply.github.com>
2024-12-03 15:15:08 -0500

    bug fixes

--- CatLib Developer v0.1/src/catLib/drivetrain/drivetrain.cpp
+++ CatLib Developer v0.1/src/catLib/drivetrain/drivetrain.cpp
@@ -1,25 +1,5 @@
 #include "main.h"
 
-catlib::TrackingWheel::TrackingWheel(pros::Rotation* rotation, double wheelDiameter, double offset) {
-    this->rotation = rotation;
-    this->wheelDiameter = wheelDiameter;
-    this->offset = offset;
-}
-
-void catlib::TrackingWheel::reset() {
-    this->rotation->reset_position();
-}
-
-double catlib::TrackingWheel::distanceTraveled() {
-    return this->rotation->get_position() / 36000 * wheelDiameter * M_PI;
-}
-
-catlib::OdomSensors::OdomSensors(pros::Imu* inertial, TrackingWheel* vertical = nullptr, TrackingWheel* horizontal = nullptr) {
-    this->inertial = inertial;
-    this->vertical = vertical;
-    this->horizontal = horizontal;
-}
-
 catlib::PIDConstants::PIDConstants(double kP, double kI, double kD) {
     this->kP = kP;
     this->kI = kI;
--- CatLib Developer v0.1/src/catLib/drivetrain/odom.cpp
+++ CatLib Developer v0.1/src/catLib/drivetrain/odom.cpp
@@ -1,1 +1,21 @@
 #include "main.h"
+
+catlib::TrackingWheel::TrackingWheel(pros::Rotation* rotation, double wheelDiameter, double offset) {
+    this->rotation = rotation;
+    this->wheelDiameter = wheelDiameter;
+    this->offset = offset;
+}
+
+void catlib::TrackingWheel::reset() {
+    this->rotation->reset_position();
+}
+
+double catlib::TrackingWheel::distanceTraveled() {
+    return this->rotation->get_position() / 36000 * wheelDiameter * M_PI;
+}
+
+catlib::OdomSensors::OdomSensors(pros::Imu* inertial, TrackingWheel* vertical = nullptr, TrackingWheel* horizontal = nullptr) {
+    this->inertial = inertial;
+    this->vertical = vertical;
+    this->horizontal = horizontal;
+}
