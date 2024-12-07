7867934e710e5bea02cc5ce5f270e7f8d78fe5d3
StevenQQQian <114100226+StevenQQian@users.noreply.github.com>
2024-12-06 23:22:36 -0500

    Update curve.hpp

--- include/catLib/pursuit/curve.hpp
+++ include/catLib/pursuit/curve.hpp
@@ -1,10 +1,11 @@
 #pragma once
 #include "Eigen/Dense"
+using namespace Eigen;
 
 namespace catlib {
     class Curve {
         public:
-            Curve(Eigen::Vector2d controlPoint1, Eigen::Vector2d controlPoint2, Eigen::Vector2d controlPoint3, Eigen::Vector2d ControlPoint4);
+            Curve(Vector2d controlPoint1, Vector2d controlPoint2, Vector2d controlPoint3, Vector2d ControlPoint4);
 
             Eigen::Vector2d getPoint(double t);
 
