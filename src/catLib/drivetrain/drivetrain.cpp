86f09640acaf26353e914e2683d12bc7283ac346
Michael Zheng <zhengm58@gmail.com>
2024-12-06 20:08:21 -0700

    Nuke

--- src/catLib/drivetrain/drivetrain.cpp
+++
@@ -1,8 +0,0 @@
-#include "catLib/drivetrain/drivetrain.hpp"
-
-catlib::Drivetrain::Drivetrain(pros::MotorGroup* leftMotors, pros::MotorGroup* rightMotors, double wheelDiameter, double driveRPM) {
-    this->leftMotors = leftMotors;
-    this->rightMotors = rightMotors;
-    this->wheelDiameter = wheelDiameter;
-    this->driveRPM = driveRPM;
-}
